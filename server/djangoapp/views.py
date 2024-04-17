ab27de37b5e8739a293bde877345bfa30ad4dd35
MaiZure <maizure@yahoo.com>
2024-04-17 20:43:45 +1200

    Lint

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -85,7 +85,7 @@ def registration(request):
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 def get_dealerships(request, state="All"):
-    if(state == "All"):
+    if (state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+state
@@ -142,4 +142,4 @@ def get_cars(request):
     for car_model in car_models:
         cars.append({"CarModel": car_model.name,
                      "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
+    return JsonResponse({"CarModels": cars})
